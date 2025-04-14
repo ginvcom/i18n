6dff5a1892d8bf939fc798d42a5790adc8f0a61b
lhcGinv <530176577@qq.com>
2025-04-14 21:55:09 +0800

    feat: fix error log

--- i18n.go
+++ i18n.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 
 	"github.com/nicksnyder/go-i18n/v2/i18n"
+	"github.com/zeromicro/go-zero/core/logx"
 	"golang.org/x/text/language"
 	"google.golang.org/grpc/metadata"
 	"gopkg.in/yaml.v3"
@@ -28,32 +29,32 @@ func RegisterLanguages(languages ...string) {
 
 }
 
-func Localize(lang string, msgId string, templateData ...interface{}) string {
+func Localize(lang string, msgId string, templateData ...interface{}) (msg string, err error) {
 	if lang == "" {
 		lang = "en"
 	}
 
 	localizer := i18n.NewLocalizer(bundle, lang)
-	var msg string
-	var err error
 
 	if templateData == nil {
 		msg, err = localizer.Localize(&i18n.LocalizeConfig{
 			MessageID: msgId,
 		})
+		if err != nil {
+			return "", fmt.Errorf("localization failed: %w", err)
+		}
 	} else {
 		msg, err = localizer.Localize(&i18n.LocalizeConfig{
 			MessageID:    msgId,
 			TemplateData: templateData[0],
 		})
+
+		if err != nil {
+			return "", fmt.Errorf("localization failed with template data: %w", err)
+		}
 	}
 
-	if err != nil {
-		fmt.Println("unspecified", templateData[0], err)
-		return "unspecified error occured"
-	}
-
-	return msg
+	return
 }
 
 // A Lang represents a lang.
@@ -74,22 +75,41 @@ func WithContext(ctx context.Context) Lang {
 	}
 }
 
+// Localize retrieves the localized message for the given message ID and template data
+// It first tries to get the language from the context metadata
+// If no language is found, it defaults to English and returns an error message
+// Returns the localized message string
 func (l *lang) Localize(msgId string, templateData ...interface{}) string {
 	md, ok := metadata.FromIncomingContext(l.ctx)
 	if !ok {
-		return Localize("en", "error.missingBasicParameter", map[string]string{"param": "lang"})
+		msg, err := Localize("en", "error.missingBasicParameter", map[string]string{"param": "lang"})
+		if err != nil {
+			logx.WithContext(l.ctx).Errorf("localization failed: %v", err)
+			return err.Error()
+		}
+
+		return msg
 	}
 
-	// 获取lang
+	// get lang
 	lang := ""
 	envArr := md.Get("lang")
 	if len(envArr) >= 1 {
 		lang = envArr[0]
 	}
 
-	return Localize(lang, msgId, templateData...)
+	msg, err := Localize(lang, msgId, templateData...)
+	if err != nil {
+		logx.WithContext(l.ctx).Errorf("localization failed: %v", err)
+		return err.Error()
+	}
+
+	return msg
 }
 
+// Error creates a new error with a localized error message
+// It prepends "error." prefix to the message ID and uses Localize to get the message
+// Returns an error with the localized message
 func (l *lang) Error(removedPrefixMsgId string, templateData ...interface{}) error {
 	prefix := "error."
 	msg := l.Localize(prefix+removedPrefixMsgId, templateData...)
